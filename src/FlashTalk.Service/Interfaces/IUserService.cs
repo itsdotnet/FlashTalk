d88706246367f81cdc36df8aebc641b2e7ec8783
iskandar kodirov <eiscocyber@gmail.com>
2023-09-15 14:24:57 +0500

    UserService prepared to  write

---
+++ src/FlashTalk.Service/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using FlashTalk.Service.DTOs.Users;
+
+public interface IUserService
+{
+    Task<bool> DeleteAsync(long id);
+    Task<UserResultDto> GetByIdAsync(long id);
+    Task<IEnumerable<UserResultDto>> GetAllAsync();
+    Task<UserResultDto> ModifyAsync(UserUpdateDto dto);
+    Task<UserResultDto> CreateAsync(UserCreationDto dto);
+    Task<UserResultDto> GetByUsernameAsync(string username);
+    Task<bool> CheckUserAsync(string username, string password);
+    Task<UserResultDto> ModifyPasswordAsync(long id, string oldPass, string newPass);
+}
