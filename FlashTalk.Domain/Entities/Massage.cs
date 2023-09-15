b2c2c349e581bc747ca178402ae4e48ffbe52746
iskandar kodirov <eiscocyber@gmail.com>
2023-09-15 10:57:47 +0500

    Entities writed

---
+++ FlashTalk.Domain/Entities/Massage.cs
@@ -0,0 +1,10 @@
+﻿using FlashTalk.Domain.Commons;
+
+namespace FlashTalk.Domain.Entities;
+
+public class Massage : Auditable
+{
+    public long From { get; set; }
+    public long To { get; set; }
+    public string Text { get; set; }
+}
---
+++ FlashTalk.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using FlashTalk.Domain.Commons;
+
+namespace FlashTalk.Domain.Entities;
+
+public class User : Auditable
+{
+    public string Name { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
