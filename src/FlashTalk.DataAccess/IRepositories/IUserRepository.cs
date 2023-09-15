ead1944ebd49a983cc13b42c01711e70ad9008d9
iskandar kodirov <eiscocyber@gmail.com>
2023-09-15 11:23:18 +0500

    UserRepository interface added

---
+++ src/FlashTalk.DataAccess/IRepositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlashTalk.Domain.Entities;
+
+namespace FlashTalk.DataAccess.IRepositories;
+
+public interface IUserRepository
+{
+    Task<User> GetAllAsync();
+    Task<User> DeleteAsync(long id);
+    Task<User> GetByIdAsync(long id);
+    Task<User> UpdateAsync(User user);
+    Task<User> CreateAsync(User user);
+    Task<User> GetByUsernameAsync(string username);
+}
