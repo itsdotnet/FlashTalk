4f2cbf7c73a91558288bc49ad1fc61c62df04480
iskandar kodirov <eiscocyber@gmail.com>
2023-09-15 11:07:24 +0500

    AppDbContext Added

---
+++ FlashTalk.DataAccess/Contexts/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace FlashTalk.DataAccess;
+
+public class AppDbContext : IDisposable
+{
+    private readonly string _connectionString;
+    private SqlConnection _connection;
+
+    public AppDbContext()
+    {
+        _connectionString = connectionString;
+    }
+
+    private SqlConnection OpenConnection()
+    {
+        if (_connection == null)
+        {
+            _connection = new SqlConnection(_connectionString);
+            _connection.Open();
+        }
+        else if (_connection.State == ConnectionState.Closed)
+        {
+            _connection.Open();
+        }
+        return _connection;
+    }
+
+    public void Dispose()
+    {
+        if (_connection != null)
+        {
+            _connection.Dispose();
+            _connection = null;
+        }
+    }
+}
