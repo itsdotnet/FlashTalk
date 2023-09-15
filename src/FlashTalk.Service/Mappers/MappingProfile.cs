d113f73c357abd4dfc84c12da29d045b8fe52477
iskandar kodirov <eiscocyber@gmail.com>
2023-09-15 14:30:26 +0500

    Mapper Added

---
+++ src/FlashTalk.Service/Mappers/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using FlashTalk.Domain.Entities;
+using FlashTalk.Service.DTOs.Messages;
+using FlashTalk.Service.DTOs.Users;
+
+namespace FlashTalk.Service.Mappers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        //User
+
+        CreateMap<User, UserResultDto>().ReverseMap();
+        CreateMap<UserUpdateDto, User>().ReverseMap();
+        CreateMap<UserCreationDto, User>().ReverseMap();
+
+        //Message
+
+        //Message
+        CreateMap<Message, MessageResultDto>().ReverseMap();
+        CreateMap<MessageUpdateDto, Message>().ReverseMap();
+        CreateMap<MessageCreationDto, Message>().ReverseMap();
+    }
+}
